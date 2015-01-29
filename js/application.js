9c5537f50f7938f050bca6562a85eef91824fc7c
amarriner <amarriner@amarriner.com>
2015-01-29 16:36:30 -0500

    First draft of skill lookup, using typehead.js

--- js/application.js
+++ js/application.js
@@ -48,10 +48,7 @@ function registerPartials() {
  * Build search controls after loading JSON
  */ 
 function activateSearch() {    
-    if (typeof json != "undefined") {
-        personaQ = json;
-    }
-                                                                    
+                                                    
     $("#select").html(Templates["search-controls"]({
         exclaim: "Select up to three personas"
     }));
@@ -120,6 +117,59 @@ function activatePersonas() {
 
 }
 
+/*
+ * Build skill search controls
+ */ 
+function activateSkills() {    
+    $("#fusion-result").empty();
+    
+    $("#select").html(Templates["skill-controls"]({
+        exclaim: "Start typing a skill name to search"
+    }));
+
+    $("#typeahead-container .typeahead").typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 2
+    },
+    {
+        name: 'skills',
+        displayKey: 'value',
+        source: substringMatcher(personaQ["skills"])
+    });
+                
+    $("#skill-search-text-field").focus().bind('typeahead:selected', function() {
+        showSkillModal($(this).val());
+    });
+}
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+ 
+    // an array that will be populated with substring matches
+    matches = [];
+ 
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+ 
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+ 
+    cb(matches);
+  };
+};
+
+/*
+ * Put appropriate skill details in modal, then show it
+ */
 function showSkillModal(skillName) {
     $("#skill-modal-content").html(Templates["skill-details"]({
         skill: getSkillByName(skillName)
